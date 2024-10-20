f9a2f4627ae4891cba55232fe6d799aaf0b7e354
wooju <woojukwon8@gmail.com>
2024-10-20 18:25:53 +0900

    base structure of weather_screen

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
 
 class WeatherScreen extends StatefulWidget {
   final dynamic parseWeatherData;
@@ -32,17 +36,79 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(cityName),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Text('$temperature'),
-          ],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.near_me),
+          iconSize: 30.0,
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.location_searching),
+            iconSize: 30.0,
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Image.asset(
+            'image/background.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text('city'),
+                    Row(
+                      children: [
+                        Text('time'),
+                        Text('day'),
+                        Text('date'),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('temperature'),
+                    Row(
+                      children: [
+                        Text('weatherImage'),
+                        Text('description'),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Divider(
+                      height: 15.0,
+                      thickness: 2.0,
+                      color: Colors.white30,
+                    ),
+                    Row(
+                      children: [
+                        Column(),
+                        Column(),
+                        Column(),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
